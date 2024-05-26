6e18b094b7a4b1fd8b8bac85b23ce91b47e32fc4
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-05-26 20:20:23 -0300

    Add files via upload

--- mapa_distribuicao_especies_vegetais.R
+++ mapa_distribuicao_especies_vegetais.R
@@ -96,7 +96,7 @@ geom_sf(data = coords_sf_brazil, aes(color = species), size = 2,
        x = "Longitude",
        y = "Latitude",
        colour = "",
-       caption = "*GBIF—the Global Biodiversity Information Facility—is an international network\n and data infrastructure funded by the world's governments\n and aimed at providing anyone, anywhere, open access to data about all\n types of life on Earth.") +
+       caption = "*GBIF—the Global Biodiversity Information Facility—is an international network and data infrastructure funded by\n the world's governments and aimed at providing anyone, anywhere, open access\n to data about all types of life on Earth.") +
   theme_minimal() +
   theme(legend.position = c(0.27, 0.35),
         axis.text = element_text(color = "black",size = 8),
